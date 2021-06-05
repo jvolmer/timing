c984bd90aa92a181bb4789aed0f714447e2c2d36
jvolmer <juliavolmer@t-online.de>
2021-06-05 15:59:03 +0200

    Separate harvest stuff and domain objects with modules

---
+++ src/projects/mod.rs
@@ -0,0 +1,12 @@
+pub mod harvest;
+
+#[derive(Debug, PartialEq)]
+pub struct Project {
+    id: u32,
+    name: String
+}
+    
+#[derive(Debug, PartialEq)]
+pub struct Projects {
+    projects: Vec<Project>,
+}
