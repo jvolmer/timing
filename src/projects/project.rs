bf4af51bd025e035b41618b614c357adc02efe6d
jvolmer <juliavolmer@t-online.de>
2021-06-27 13:02:32 +0200

    Make find task functionality available for a project

--- src/projects/project.rs
+++ src/projects/project.rs
@@ -1,5 +1,8 @@
 use crate::projects::tasks::{Tasks, TasksBuilder};
 use crate::projects::named::Named;
+use crate::projects::task::Task;
+use crate::projects::project_error::ProjectError;
+use crate::projects::list_with_names::ListWithNames;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Project {
@@ -11,6 +14,35 @@ pub struct Project {
 impl Named for Project {
     fn name(&self) -> &str {
 	&self.name
+    }
+}
+
+impl Project {
+    fn find_task(&self, search_string: &str) -> Result<&Task, ProjectError> {
+	self.tasks.find(search_string)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::projects::task::TaskBuilder;
+
+    #[test]
+    fn it_finds_task_with_specific_string() {
+	let expected_task = TaskBuilder::new()
+	    .with_name("Task with some speciality".to_string())
+	    .build();
+	let project = ProjectBuilder::new()
+	    .with_tasks(TasksBuilder::new()
+			.with_tasks(vec![
+			    expected_task.clone()])
+			.build())
+	    .build();
+	
+	let task = project.find_task("Special");
+
+	assert_eq!(task, Ok(&expected_task));
     }
 }
 
